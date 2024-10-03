1183b4de8935829a767b353742cc9b73a102e3ee
Jnandeep Talukdar <71124007+spilltheT@users.noreply.github.com>
2024-10-03 14:44:04 +0200

    non-dimensional lubrication with surfactant

--- lubrication.py
+++ lubrication.py
@@ -2,22 +2,23 @@ from pyoomph import *
 from pyoomph.expressions import *
 
 class LubricationEquations(Equations):
-	def __init__(self,sigma=1,mu=1,disjoining_pressure=0):
+	def __init__(self,Ca=1,Ma=1):
 		super(LubricationEquations, self).__init__()
-		self.sigma=sigma
-		self.mu=mu
-		self.disjoining_pressure=disjoining_pressure
+		self.Ca=Ca
+		self.Ma=Ma
 		
 	def define_fields(self):
 		self.define_scalar_field("h","C2")
 		self.define_scalar_field("p","C2")		
+		self.define_scalar_field("Gamma","C2")
 		
 	def define_residuals(self):
 		h,eta=var_and_test("h")
-		p,q=var_and_test("p")		
-		self.add_residual(weak(partial_t(h),eta)+weak(1/self.mu*(h**3/3*grad(p)-h**2/2*grad(self.sigma)),grad(eta)))
-		self.add_residual(weak(p-self.disjoining_pressure,q)-weak(self.sigma*grad(h),grad(q)))
-		
+		p,q=var_and_test("p")
+		Gamma,phi=var_and_test("Gamma")
+		self.add_residual(weak(partial_t(h),eta) + weak((h**3/3*grad(p) - h**2/2*grad(Gamma)),grad(eta)))
+		self.add_residual(weak(self.Ca*p,q)-weak(grad(h) + self.Ca*Gamma*grad(h),grad(q)))
+		self.add_residual(weak(partial_t(Gamma),phi) - weak((h*Gamma*grad(Gamma) - h**2/2*Gamma*grad(p)) - 1/self.Ma*grad(Gamma),grad(phi)))
 		
 class LubricationProblem(Problem):	
 	def define_problem(self):
@@ -26,7 +27,6 @@ class LubricationProblem(Problem):
 		eqs+=TextFileOutput() # output	
 		eqs+=InitialCondition(h=0.05*(1+0.25*cos(2*pi*var("coordinate_x"))))  # small height with a modulation
 		self.add_equations(eqs@"domain") # adding the equation
-
 		
 if __name__=="__main__":
 	with LubricationProblem() as problem:
