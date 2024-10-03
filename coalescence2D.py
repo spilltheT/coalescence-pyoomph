e30415a43dce7b394bce114a9b2372322516140d
Jnandeep Talukdar <71124007+spilltheT@users.noreply.github.com>
2024-10-03 14:42:46 +0200

    convergence issues: 1D lubrication with surfactants

--- coalescence2D.py
+++ coalescence2D.py
@@ -1,9 +1,17 @@
+import sys
+from pyoomph.materials import * 
+import pyoomph.materials.default_materials
+
 from pyoomph.generic.problem import Problem
 from pyoomph.meshes.meshdatacache import MeshDataEigenModes
 from pyoomph.typings import List, Optional, Union
 from lubrication_spreading import * 
 from pyoomph.output.plotting import *
 from pyoomph.output.meshio import TextFileOutputAlongLine
+
+
+from pyoomph.expressions.units import *  # units
+from pyoomph.expressions.phys_consts import gas_constant  # and the gas constant
 
 class PlotterTry(MatplotlibPlotter):
 	def __init__(self, problem: Problem, filetrunk: str = "plot_{:05d}", fileext: str | List[str] = "png", eigenvector: int | None = None, eigenmode: MeshDataEigenModes = "abs", add_eigen_to_mesh_positions: bool = True, position_eigen_scale: float = 1):
@@ -20,36 +28,47 @@ class PlotterTry(MatplotlibPlotter):
 class DropletCoalescence(DropletSpreading):	
 	def __init__(self):
 		super(DropletCoalescence,self).__init__()
-		self.distance=2.0
-		self.hp=0.001
-		self.h_center=0.2
-		self.Lx=7.5
+		self.L=1
+		self.theta=10*pi/180
+		self.R=self.L/(sin(self.theta))
+		self.H=self.R-self.L/(tan(self.theta))
+		self.hp=0.0001
+		self.Lx=6
 		self.max_refinement_level=6
+
+		self.Ca=1e0
+		self.Ma=float(sys.argv[1])
 		# self.plotter = PlotterTry(self)
 			
 					
 	def define_problem(self):
-		self.add_mesh(LineMesh(N=500,size=self.Lx)) 
+		self.add_mesh(LineMesh(minimum=-3,size=self.Lx, N=1000)) 
 		
 		h=var("h") 
+		Gamma=var("Gamma")
 
-		disjoining_pressure=0
+		# self.sigma=self.sigma-self.ma*Gamma
 
-		eqs=LubricationEquations(sigma=self.sigma,disjoining_pressure=disjoining_pressure) # equations
+		eqs=LubricationEquations(Ca=self.Ca,Ma=self.Ma) # equations
 		eqs+=MeshFileOutput() # output	
 		x=var("coordinate")
-		h1=self.h_center*(1-((var("coordinate_x")+1)/self.R)**2) # height functions of the droplets
-		h2=self.h_center*(1-((var("coordinate_x")-1)/self.R)**2)		
-		h_init=h_init=maximum(maximum(h1,h2),self.hp) # Initial height: maximum of h1, h2 and precursor
+
+		h1=-self.R + self.H + (self.R**2 - (var("coordinate_x") + (2 * self.R * self.H - self.H**2)**(0.5))**2)**(0.5)
+		h2=-self.R + self.H + (self.R**2 - (var("coordinate_x") - (2 * self.R * self.H - self.H**2)**(0.5))**2)**(0.5)
+		h_init=h_init=maximum(maximum(h1,h2),self.hp) 
+
+		Gamma_init = Gamma_init = -0.5*tanh(1/self.hp * var("coordinate_x")) + 0.5
+		
+		eqs+=InitialCondition(Gamma=Gamma_init)
 		eqs+=InitialCondition(h=h_init) 
 		
 		eqs+=SpatialErrorEstimator(h=1) # refine based on the height field
+		eqs += TextFileOutput()
 
 		# self+=TextFileOutputAlongLine(filename='profile', start=(-3,0), end=(3,0), N =200) @ "domain"
 		
 		self.add_equations(eqs@"domain") # adding the equation
-
 		
 if __name__=="__main__":
 	with DropletCoalescence() as problem:
-		problem.run(100,outstep=0.1,startstep=0.01,maxstep=10,temporal_error=1,spatial_adapt=1)
+		problem.run(100,outstep=0.1,startstep=0.01,maxstep=50,temporal_error=1,spatial_adapt=1)
